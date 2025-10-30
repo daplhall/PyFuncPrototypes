6cda4be32c1f71470428cb6cd8a84e383b8f3ae9
Daniel Ploug Hall <danielploug@hotmail.com>
2025-10-30 11:58:32 +0100

    [iss-13] forgot to update test to new protocol

--- tests/test_prototype.py
+++ tests/test_prototype.py
@@ -4,34 +4,36 @@ import pyprototypes.exceptions as exceptions
 
 
 def test_prototype_success(base_prototype):
+	@base_prototype.function
 	def inpt(potato, pizza): ...
 
-	assert base_prototype.check(inpt) == inpt
+	assert base_prototype.check(inpt)
 
 
 def test_prototype_name_fail(base_prototype):
-	def inpt(potat, piz): ...
-
 	with pytest.raises(
 		exceptions.UnsupportedParameters,
 		match=r".*Parameter 'potat' is not supported, did you mean:.*",
 	):
-		base_prototype.check(inpt)
+
+		@base_prototype.function
+		def inpt(potat, piz): ...
 
 
 def test_prototype_type_failure(typed_prototype):
-	def inpt(pizza: int, potato: float): ...
-
 	with pytest.raises(
 		exceptions.UnsupportedParameters,
 		match=r".*Wrong Type - Parameter 'pizza'.*",
 	):
-		typed_prototype.check(inpt)
+
+		@typed_prototype.function
+		def inpt(pizza: int, potato: float): ...
 
 
 def test_fixture_fixtures_success(fixture_prototype):
+	@fixture_prototype.function
 	def inpt(pizza, potato):
 		return pizza + potato
 
-	p = fixture_prototype.check(inpt)
-	assert p() == 104
+	assert fixture_prototype.check(inpt)
+	assert inpt() == 104
